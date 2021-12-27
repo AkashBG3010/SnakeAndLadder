30c2b7c87325b2df8e8029c31abfd42cca8ebb79
akashbg3010 <akashbpura@gmail.com>
2021-12-27 20:54:41 +0530

    Updated the player position to 0

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,7 +4,9 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {	
 		int die = 0;
+		int start = 0;
 			die = (int)(Math.random()*6) + 1;
 			System.out.println("Random number is: " + die );
+			System.out.println("Starting position of player is: " + start);
          }
 }
