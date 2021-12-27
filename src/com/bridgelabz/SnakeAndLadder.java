63485ba1b639691297d0b8ab0c480486702b8d65
akashbg3010 <akashbpura@gmail.com>
2021-12-27 20:17:23 +0530

    Added the random dice

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {	
+		int die = 0;
+			die = (int)(Math.random()*6) + 1;
+			System.out.println("Random number is: " + die );
+         }
+}
